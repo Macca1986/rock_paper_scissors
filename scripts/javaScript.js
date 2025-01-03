a2a962ef1595388ffa62efe097f821c4d07c4eed
Macca1986 <matthew_mack86@hotmail.com>
2025-01-03 14:05:17 +0400

    Write logic to play entire game
    Created a function to play 5 rounds of scissors, paper, rock and
    log the winner of the game in the console

--- scripts/javaScript.js
+++ scripts/javaScript.js
@@ -1,12 +1,10 @@
-console.log("Hello World");
-
 // create function that gets the computers choice
 function getComputerChoice() {
     let randNum = Math.random();
-    console.log(randNum);
+    // console.log(randNum);
     // multiply by three for choosing from the three options
     let choiceNum = randNum * 3;
-    console.log(choiceNum);
+    // console.log(choiceNum);
     // choose rock paper or scissors based on the above using conditionals
     if (choiceNum < 1) {
         return "rock";
@@ -23,36 +21,53 @@ function getHumanChoice() {
     return humanChoice;
 }
 
-// initialise variable to keep scores
-let humanScore = 0;
-let computerScore = 0;
 
-// write logic to play a single round
-function playRound(humanChoice, computerChoice) {
-     if (humanChoice == computerChoice) {
+
+// write logic to play 5 rounds of the game
+function playGame() {
+    // initialise variable to keep scores
+    let humanScore = 0;
+    let computerScore = 0;
+
+    // write logic to play a single round
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice == computerChoice) {
+        console.log("It's a draw! You both chose " + humanChoice);
+        } else if (humanChoice == "rock" && computerChoice == "scissors") {
+        console.log("You win! " + humanChoice + " beats " + computerChoice);
+        humanScore += 1;
+        } else if (humanChoice == "rock" && computerChoice == "paper") {
+        console.log("You lose! " + computerChoice + " beats " + humanChoice);
+        computerScore += 1;
+        } else if (humanChoice == "paper" && computerChoice == "rock") {
+        console.log("You win! " + humanChoice + " beats " + computerChoice);
+        humanScore += 1;
+        } else if (humanChoice == "paper" && computerChoice == "scissors") {
+        console.log("You lose! " + computerChoice + " beats " + humanChoice);
+        computerScore += 1;
+        } else if (humanChoice == "scissors" && computerChoice == "paper") {
+        console.log("You win! " + humanChoice + " beats " + computerChoice);
+        humanScore += 1;
+        } else if (humanChoice == "scissors" && computerChoice == "rock") {
+        console.log("You lose! " + computerChoice + " beats " + humanChoice);
+        computerScore += 1;
+        }
+        console.log("The score is Human: " + humanScore + " to Computer: " + computerScore);
+    }
+
+    // play the game 5 times, prompting for user and computer choices each round
+    for (let i = 0; i < 5; i++) {
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
+    
+    // display result message after 5 rounds are complete
+    if (humanScore > computerScore) {
+        console.log("Congratulations, you win!");
+    } else if (computerScore > humanScore) {
+        console.log("Sorry, you lose!");
+    } else {
         console.log("It's a draw!");
-     } else if (humanChoice == "rock" && computerChoice == "scissors") {
-        console.log("You win! " + humanChoice + " beats " + computerChoice);
-        humanScore += 1;
-     } else if (humanChoice == "rock" && computerChoice == "paper") {
-        console.log("You lose! " + computerChoice + " beats " + humanChoice);
-        computerScore += 1;
-     } else if (humanChoice == "paper" && computerChoice == "rock") {
-        console.log("You win! " + humanChoice + " beats " + computerChoice);
-        humanScore += 1;
-     } else if (humanChoice == "paper" && computerChoice == "scissors") {
-        console.log("You lose! " + computerChoice + " beats " + humanChoice);
-        computerScore += 1;
-     } else if (humanChoice == "scissors" && computerChoice == "paper") {
-        console.log("You win! " + humanChoice + " beats " + computerChoice);
-        humanScore += 1;
-     } else if (humanChoice == "scissors" && computerChoice == "rock") {
-        console.log("You lose! " + computerChoice + " beats " + humanChoice);
-        computerScore += 1;
-     }
+    }
 }
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
