35e5caee893dab163027f4f4e3372247a7138dc6
Macca1986 <matthew_mack86@hotmail.com>
2025-01-03 13:50:58 +0400

    Create logic to play one round
    Created and tested a function to play one round of scissors, paper
    rock

--- scripts/javaScript.js
+++ scripts/javaScript.js
@@ -26,3 +26,33 @@ function getHumanChoice() {
 // initialise variable to keep scores
 let humanScore = 0;
 let computerScore = 0;
+
+// write logic to play a single round
+function playRound(humanChoice, computerChoice) {
+     if (humanChoice == computerChoice) {
+        console.log("It's a draw!");
+     } else if (humanChoice == "rock" && computerChoice == "scissors") {
+        console.log("You win! " + humanChoice + " beats " + computerChoice);
+        humanScore += 1;
+     } else if (humanChoice == "rock" && computerChoice == "paper") {
+        console.log("You lose! " + computerChoice + " beats " + humanChoice);
+        computerScore += 1;
+     } else if (humanChoice == "paper" && computerChoice == "rock") {
+        console.log("You win! " + humanChoice + " beats " + computerChoice);
+        humanScore += 1;
+     } else if (humanChoice == "paper" && computerChoice == "scissors") {
+        console.log("You lose! " + computerChoice + " beats " + humanChoice);
+        computerScore += 1;
+     } else if (humanChoice == "scissors" && computerChoice == "paper") {
+        console.log("You win! " + humanChoice + " beats " + computerChoice);
+        humanScore += 1;
+     } else if (humanChoice == "scissors" && computerChoice == "rock") {
+        console.log("You lose! " + computerChoice + " beats " + humanChoice);
+        computerScore += 1;
+     }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
