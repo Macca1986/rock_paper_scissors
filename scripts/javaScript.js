b2b9b5e4341473c3eeeb9d84a622186873e171a6
Macca1986 <matthew_mack86@hotmail.com>
2025-01-03 13:31:18 +0400

    Create getComputerChoice function
    Created a function to have the computer generate a choice between
    rock, paper and scissors randomly

--- scripts/javaScript.js
+++ scripts/javaScript.js
@@ -1,1 +1,18 @@
 console.log("Hello World");
+
+// create function that gets the computers choice
+function getComputerChoice() {
+    let randNum = Math.random();
+    console.log(randNum);
+    // multiply by three for choosing from the three options
+    let choiceNum = randNum * 3;
+    console.log(choiceNum);
+    // choose rock paper or scissors based on the above using conditionals
+    if (choiceNum < 1) {
+        return "rock";
+    } else if (choiceNum < 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
